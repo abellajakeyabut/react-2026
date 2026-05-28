12d31af52ee08fb0474833f9941378a3c361e69b
Jake Abella <jakeabella@Jakes-MacBook-Air.local>
2026-05-28 23:21:24 +0800

    push miniair

--- react-course-v5/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ react-course-v5/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,40 @@
+import {useState} from 'react'
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [inputObj, setInput] = useState({})
+  const handleChanges = (e) =>{
+    e.preventDefault();
+    let inputObjProxy = {...inputObj,[e.target.id]:e.target.value}
+    setInput(inputObjProxy)
+  }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(`submitted name:${inputObj.name}, value:${inputObj.email}`)
+  }
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <h4>Controlled Inputs</h4>
+      <div className='form-row'>
+        <label htmlFor='name' className='form-label'>
+          Name
+        </label>
+        <input type='text' id='name' className='form-input' onChange={(e)=>{handleChanges(e)}}/>
+  
+      </div>
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          Email
+        </label>
+        <input type='text' id='email' className='form-input' onChange={(e)=>{handleChanges(e)}}/>
+        
+      </div>
+      <div className='form-row'>
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </div>
+      
+    </form>
+  )
 };
 export default ControlledInputs;
