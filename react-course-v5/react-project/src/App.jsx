a78245c214636c3a683404b7e9ec38b9e591ad16
Jake Abella <jakeabella@Jakes-MacBook-Air.local>
2026-05-28 14:15:07 +0800

    mini air

---
+++ react-course-v5/react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import List from '../../03-advanced-react/src/tutorial/05-leverage-javascript/starter/List'
+
+
+function App() {
+  return (
+    <>
+    
+      <List></List>
+    </>
+  )
+}
+
+export default App
