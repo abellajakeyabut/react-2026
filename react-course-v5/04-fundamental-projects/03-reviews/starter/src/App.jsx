93a15d346e188a3ab1f7888389105f34c597bc1f
root <abellajakeyabut@gmail.com>
2026-07-01 02:42:27 +0800

    04

--- react-course-v5/04-fundamental-projects/03-reviews/starter/src/App.jsx
+++ react-course-v5/04-fundamental-projects/03-reviews/starter/src/App.jsx
@@ -1,4 +1,36 @@
+import { useState } from 'react';
+import data from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import './index.css'
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0)
+  const randomPerson = ()=> {
+    let randomNumber = Math.floor(Math.random() * data.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    if(randomNumber > data.length-1){
+      randomNumber = 0;
+    }
+    setIndex(randomNumber)
+  }
+  console.log(data);
+  return (
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={data[index].image} className="person-img" />
+           <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+        </div>
+       
+        <h4 className="author">{data[index].name}</h4>
+        <p className="job">{data[index].job}</p>
+        <p className="info">{data[index].text}</p>
+        <button className="btn" onClick={randomPerson}>Surprise Me</button>
+      </article>
+    </main>
+  )
 };
 export default App;
